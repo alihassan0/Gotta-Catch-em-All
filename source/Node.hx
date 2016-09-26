24d5400a60c97411e50e8ca8e19c11d39cba5524
Abdelrahman Mahmoud <a.mahmoud1994@gmail.com>
2016-09-26 22:12:10 +0200

    Adding constructors for Node class

--- source/Node.hx
+++ source/Node.hx
@@ -7,4 +7,13 @@ class Node
 
 	var state: State;
 	var parent: Node;
+
+	public function new() { }
+
+	public function new(state: State, parent: Node) 
+	{
+	this.state  = state;
+	this.parent = parent;
+	}
+
 }
