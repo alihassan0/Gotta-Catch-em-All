7ef527befea85121aa3447a7259736f2f38bfa56
Ali Hassan <alithephantom@gmail.com>
2016-10-03 00:22:12 +0200

    Add regenerate buttom

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -8,25 +8,46 @@ import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
 
 import ai.*;
+import util.*;
 import gui.*;
 
 class MenuState extends FlxState
 {
 	var maze:Maze;
 	var guiMaze:GuiMaze;
+	
+
+	//gui
+	var randomizeBtn:FlxButton;
 
 	override public function create():Void
 	{
 		super.create();
-		maze = new Maze(19, 19);
+		maze = new Maze(9, 9);
 		guiMaze = new GuiMaze(maze);
-		MazeGenerator.generateMaze(maze);
+		MazeGenerator.generateMaze(maze, beginSearching);
 		
-		// MazeGenerator.traceMaze(maze);
+		randomizeBtn = new FlxButton(0, 0, "Generate", generateNewMaze);
+		add(randomizeBtn);
 
-		
 	}
 
+	function beginSearching()
+	{
+		guiMaze.updateProblemStats();
+		MazeSearcher.search(maze, Strategy.BreadthFirst ,  false);
+
+		MazeSearcher.search(maze, Strategy.DepthFirst ,  false);
+	}
+	
+	function generateNewMaze()//generate new Maze 
+	{
+		if(!maze.isGenerating)
+		{
+			maze.reset();
+			MazeGenerator.generateMaze(maze);
+		}
+	}
 	
 	override public function update(elapsed:Float):Void
 	{
